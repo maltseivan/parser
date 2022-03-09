3003016e61b1c50c10fe13a5ac126f4a6ed24dcc
maltseivan <98417972+maltseivan@users.noreply.github.com>
2022-03-09 13:01:20 +0300

    parser v1.2

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-URL = 'https://habr.com/ru/news/t/654711/'
+URL = 'https://habr.com/ru/company/first/blog/653781/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36', 'accept': '*/*'}
 
 
@@ -13,23 +13,13 @@ def get_html(url, params=None):
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     chapter = soup.find('h1', class_='tm-article-snippet__title tm-article-snippet__title_h1').find_all("span")
-    text_p = soup.find(class_="article-formatted-body").find_all('p')
-    text_h2 = soup.find(class_="article-formatted-body").find_all('h2')
-    text_h3 = soup.find(class_="article-formatted-body").find_all('h3')
+    text_p = soup.find('div', class_="tm-article-body")
 
     text = []
 
     for elem in chapter:
-        elem_text = elem.text
-        text.append(elem_text)
-
-    for elem in text_h2:
-        elem_text = elem.text
-        text.append(elem_text)
-
-    for elem in text_h3:
-        elem_text = elem.text
-        text.append(elem_text)
+        chapter_text = elem.text
+        text.append(chapter_text)
 
     for elem in text_p:
         elem_text = elem.text
@@ -39,8 +29,20 @@ def get_content(html):
     name_new = "".join(c for c in name_replace if c.isalpha())
     name = name_new + '.json'
 
+    valu = ""
+    vale = " "
+
+    while valu in text:
+        text.remove(valu)
+
+    while vale in text:
+        text.remove(vale)
+
     with open(name, 'w', encoding='utf-8') as file:
         json.dump(text, file, indent=3, ensure_ascii=False)
+
+    for item in text:
+        print(item)
 
 def parse():
     result = get_html(URL)
